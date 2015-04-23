3b1d34307b7257463fb8897dfec75d4fefe72cfb
Javier Lopez <m@javier.io>
2015-04-23 04:06:30 -0500

    add backup-mozilla, move update-cd to minos-tools-extra

---
+++ debian/minos-tools.backup-mozilla.cron.d
@@ -0,0 +1,2 @@
+#This file is part of the minos-tools-extra package
+15 */4 * * * root /usr/bin/backup-mozilla
--- debian/minos-tools.update-cd.cron.d
+++
@@ -1,2 +0,0 @@
-#This file is part of the minos-tools package
-0 23 * * *  root /usr/bin/update-cd
